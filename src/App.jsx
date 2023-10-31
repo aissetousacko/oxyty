e77dc160d5ed4fb8e451762617ef481d5cb41244
Aïssetou SACKO <aissetou.sck@gmail.com>
2023-10-31 10:13:25 +0100

    Adding elements of the banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import Button from './components/Button'
 import Navbar from './components/Navbar'
+import imgWoman from './assets/oxyty_woman.png'
 
 function App() {
-
   return (
     <div>
       <Navbar />
-        
+      <div className="banner">
+        <div className="banner__left">
+          <h1>EN 2023, FINI LES EXCUSES !</h1>
+          <h2>FRAIS D’INSCRIPTIONS OFFERTS !</h2>
+          <p>
+            Rejoignez dès maintenant l’aventure et découvrez l’ambiance
+            familiale et conviviable qui vous attend !
+          </p>
+          {/* component button */}
+          <Button text="S'INSCRIRE" />
+        </div>
+
+        <div className="banner__right">
+          <img src={imgWoman} alt="Image woman fighter" />
+        </div>
+      </div>
     </div>
   )
 }
